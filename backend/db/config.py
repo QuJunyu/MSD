3b0e8c669b9b79c97ef00ce7ea7286b2cd2af850
QianChenwang <163571380+QianChenwang@users.noreply.github.com>
2025-10-02 10:24:35 +0800

    Create config.py

---
+++ backend/db/config.py
@@ -0,0 +1,8 @@
+# Database connection configuration (When C checks, confirm that B has been modified to actual information)
+db_config = {
+    "host": "localhost",         # Local database uses localhost by default
+    "user": "root",              # Your MySQL username (default is root)
+    "password": "20040715",        # Change to your MySQL password! Connection will fail if not changed
+    "database": "campus_book_trade",   # Database name (exclusively for login function)
+    "port": 3306                 # MySQL default port
+}
