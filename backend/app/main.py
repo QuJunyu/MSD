3df17129b9d1d0fa79eb74a6ac2a0c17523a50cf
QianChenwang <163571380+QianChenwang@users.noreply.github.com>
2025-10-05 19:37:50 +0800

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,6 @@
-# From the __init__.py in the current directory, import app
+# Import app (Flask application object) from __init__.py in the current directory
 from . import app
 
-# Start the service (run with python -m backend.app.main in the project root directory)
+# Entry point for starting: start the service when running main.py directly
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000, debug=True)
