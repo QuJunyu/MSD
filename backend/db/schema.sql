eb447022e4408d21be807918b433d3eda5935af9
QuJunyu <163547161+QuJunyu@users.noreply.github.com>
2025-10-06 00:25:15 +0800

    Delete backend directory

--- backend/db/schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE IF NOT EXISTS campus_book_trade;
-USE campus_book_trade;
-
-CREATE TABLE IF NOT EXISTS users (
-  id INT AUTO_INCREMENT PRIMARY KEY,  -- Incremental ID
-  username VARCHAR(50) UNIQUE NOT NULL,  -- User name (unique)
-  password VARCHAR(255) NOT NULL,  -- Password (for subsequent encryption and storage)
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP  -- Creation Date
-);
